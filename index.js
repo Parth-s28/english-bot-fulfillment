6e78ecf726697d599c0a1d896614933fa3cf2419
Parth-s28 <62176805+Parth-s28@users.noreply.github.com>
2021-07-14 10:02:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ const dialogflowFulfillment = (request, response) => {
 
     function sayHello(agent){
         agent.add("You deployed a fulfillment successfully.")
+        agent.add(new Card({
+         title: `Title: this is a card title`,
+         text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+         buttonText: 'Click me',
+         buttonUrl: 'https://assistant.google.com/'
+       })
+     );   
     }
 
     let intentMap = new Map();
